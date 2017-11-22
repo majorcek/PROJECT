8a30b98102d694263ee61819a0aa7c7ecf0f3a68
Andraž Pirnovar <andraz.pirnovar@gmail.com>
2017-11-22 17:41:42 +0100

    Typo fix

--- graphs/scripts/graphs_probabilities.R
+++ graphs/scripts/graphs_probabilities.R
@@ -3,7 +3,7 @@ library(reshape)
 library(dplyr)
 library(rvest)
 
-probabaility_naive0 <- read.csv2("data/probability_naive_4000.csv", header = TRUE, dec = ".", sep = ",", encoding = "UTF-8", check.names = FALSE)
+probabaility_naive0 <- read.csv2("../../data/probability_naive_4000.csv", header = TRUE, dec = ".", sep = ",", encoding = "UTF-8", check.names = FALSE)
 probabaility_naive <- melt(probabaility_naive0, id = "r")
 colnames(probabaility_naive) <- c("r", "tocke", "probability")
 
@@ -13,13 +13,13 @@ graf_probabaility_naive <- ggplot(data = probabaility_naive, aes(x = r, y = prob
   scale_colour_discrete(name="Number of\n  points\n") + 
   labs(x = "ratio between circle radiuses")+
   labs(y = "probability")+
-  labs(title = "Length of convex hull versus ratios for different number \nof points (naive distributed)")+
-  labs(caption = "*length is diveded with R of bigger circle")
+  labs(title = "Length of convex hull versus ratios for different number \nof points (naive distribution)")+
+  labs(caption = "*length is divided with R of bigger circle")
 
 ##################################################
 ##################################################
 
-probabaility_polar0 <- read.csv2("data/probability_polar_4000.csv", header = TRUE, dec = ".", sep = ",", encoding = "UTF-8", check.names = FALSE)
+probabaility_polar0 <- read.csv2("../../data/probability_polar_4000.csv", header = TRUE, dec = ".", sep = ",", encoding = "UTF-8", check.names = FALSE)
 probabaility_polar <- melt(probabaility_polar0, id = "r")
 colnames(probabaility_polar) <- c("r", "tocke", "probability")
 
@@ -29,8 +29,8 @@ graf_probability_polar <- ggplot(data = probabaility_polar, aes(x = r, y = proba
   scale_colour_discrete(name="Number of\n  points\n") + 
   labs(x = "ratio between circle radiuses")+
   labs(y = "probability")+
-  labs(title = "Length of convex hull versus ratios for different number \nof points (naive distributed)")+
-  labs(caption = "*length is diveded with R of bigger circle")
+  labs(title = "Length of convex hull versus ratios for different number \nof points (polar distribution)")+
+  labs(caption = "*length is divided with R of bigger circle")
 
 ##############################################
 ##############################################
